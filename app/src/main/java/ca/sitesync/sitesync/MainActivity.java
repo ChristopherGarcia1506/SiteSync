155f848de9011d17ecb7fe92277474e9c783cb1a
Tyler <tyler.meira@hotmail.ca>
2025-09-18 19:52:33 -0400

    Added back button functionality, with proper logo.

--- app/src/main/java/ca/sitesync/sitesync/MainActivity.java
+++ app/src/main/java/ca/sitesync/sitesync/MainActivity.java
@@ -6,10 +6,13 @@ Tyler Meira (N01432291) 0CA
 */
 package ca.sitesync.sitesync;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -32,6 +35,34 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.nav_view);
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Exit Application")
+                        .setMessage("Are you sure you want to exit?")
+                        .setIcon(R.drawable.sitesynclogo)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish(); // Closes the current activity
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+
+                setEnabled(false);
+                new MainActivity().getOnBackPressedDispatcher().onBackPressed();
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
 
         // Load default fragment (JobBoardFragment)
         if (savedInstanceState == null) {
