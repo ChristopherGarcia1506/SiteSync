23d42d69482c665ecea0f4c1d951fba4520902e9
AnthonyMancia3670 <amancia2005@gmail.com>
2025-09-20 18:55:09 -0400

    now has a animation run during the splash screen showing our logo

--- app/src/main/java/ca/sitesync/sitesync/SplashScreen.java
+++ app/src/main/java/ca/sitesync/sitesync/SplashScreen.java
@@ -10,6 +10,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,11 +21,28 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        // Views
+        ImageView logo = findViewById(R.id.logo);
+
+        //Loads in the animations
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation slideUp = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+
+        //combines both so it slides and fades in when the app starts
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(fadeIn);
+        set.addAnimation(slideUp);
+
+        logo.startAnimation(set);
+
 
         // Delay for 3 seconds, then go to MainActivity
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             startActivity(new Intent(SplashScreen.this, MainActivity.class));
             finish(); // close splash so user can't go back to it
-        }, 3000);
+        }, 3500);
     }
 }
