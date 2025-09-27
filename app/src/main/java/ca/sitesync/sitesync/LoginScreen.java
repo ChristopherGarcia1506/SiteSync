27b0fba5dd3c8c1a45e24dbd7ebf6426d394b770
Tyler <tyler.meira@hotmail.ca>
2025-09-27 19:23:43 -0400

    Added Runtime permissions for notifications (no functionality will be needed for later)

--- app/src/main/java/ca/sitesync/sitesync/LoginScreen.java
+++ app/src/main/java/ca/sitesync/sitesync/LoginScreen.java
@@ -18,10 +18,44 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class LoginScreen extends AppCompatActivity {
 
+    private void showNotification(String message, String action){
+        View parentLayout = findViewById(android.R.id.content); // Or a specific root layout like a CoordinatorLayout
+        Snackbar.make(parentLayout, message, Snackbar.LENGTH_LONG)
+                .setAction(action, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(action.equals("Try Again")){
+                            askForPermission();
+                        }
+                    }
+                })
+                .show();
+    }
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    showNotification("Permission granted", "Dismiss");
+                } else {
+                    showNotification("Permission denied", "Try Again");
+                }
+            });
+    protected void askForPermission() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+        else{
+            showNotification("Permission granted", "Dismiss");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        askForPermission();
+
     }
 }
