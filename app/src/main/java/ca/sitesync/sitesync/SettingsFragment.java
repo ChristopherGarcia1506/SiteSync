959687fd4bb1e89b5730444b6d6b34d10ef28021
Tyler <tyler.meira@hotmail.ca>
2025-09-27 20:30:37 -0400

    Added, a default profile picture, and changed Recycler view to list view, and added some settigns options with no functionality

--- app/src/main/java/ca/sitesync/sitesync/SettingsFragment.java
+++ app/src/main/java/ca/sitesync/sitesync/SettingsFragment.java
@@ -3,10 +3,17 @@ package ca.sitesync.sitesync;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,28 +22,22 @@ import android.view.ViewGroup;
  */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
@@ -58,7 +59,20 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        ListView listView = view.findViewById(R.id.listView);
+
+        List<String> items = new ArrayList<>();
+        items.add("Rotation Lock");
+        items.add("Profile Picture");
+        items.add("Permissions");
+        items.add("About");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
