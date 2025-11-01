e3e77b93c2baa579711b30f5e9514f366a5b2a0e
Chris Garcia <chrisgarcia911@outlook.com>
2025-11-01 19:09:35 -0400

    update employers home page(JoblistingsFragment), and added neede variables to job items and jobadapter

--- sitesyncmanagement/src/main/java/ca/sitesync/sitesync/JobItems.java
+++ sitesyncmanagement/src/main/java/ca/sitesync/sitesync/JobItems.java
@@ -1,17 +1,34 @@
 package ca.sitesync.sitesync;
 
 public class JobItems {
-    private String company;
-    private String description;
-    private String location;
+    private String title;
+    private String address;
+    private String status;
+    private String employerId;
+    private String jobId;
 
-    public JobItems(String company, String description, String location) {
-        this.company = company;
-        this.description = description;
-        this.location = location;
+    public JobItems() {
     }
 
-    public String getCompany() { return company; }
-    public String getDescription() { return description; }
-    public String getLocation() { return location; }
+    public JobItems(String title, String address, String status) {
+        this.title = title;
+        this.address = address;
+        this.status = status;
+    }
+    public JobItems(String title, String address, String status, String employerId) {
+        this.title = title;
+        this.address = address;
+        this.status = status;
+        this.employerId = employerId;
+    }
+    // Getters
+    public String getTitle() { return title; }
+    public String getAddress() { return address; }
+    public String getStatus() { return status; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAddress(String address) { this.address = address; }
+    public void setStatus(String status) { this.status = status; }
+    public void setEmployerId(String employerId) { this.employerId = employerId; }
+    public void setJobId(String jobId) { this.jobId = jobId; }
 }
