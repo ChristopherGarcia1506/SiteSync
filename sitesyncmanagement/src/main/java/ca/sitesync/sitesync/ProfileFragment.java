699ceb5dd31921625ae71763474b2cb2a230122f
Chris Garcia <chrisgarcia911@outlook.com>
2025-11-16 15:24:24 -0500

    Profile Fields are now visible to their respective user

--- sitesyncmanagement/src/main/java/ca/sitesync/sitesync/ProfileFragment.java
+++ sitesyncmanagement/src/main/java/ca/sitesync/sitesync/ProfileFragment.java
@@ -10,44 +10,32 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,16 +43,52 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+
+            TextView usersFullName = view.findViewById(R.id.UsersName);
+            TextView usersEmail = view.findViewById(R.id.UserEmail);
+            TextView usersPhoneNo = view.findViewById(R.id.UserPhone);
+            TextView usersOrganization = view.findViewById(R.id.UserOrganization);
+
+            db.collection("Accounts")
+                    .document(userId)  // Use document() instead of where() if userId is the document ID
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // Get data from document
+                                String fullName = document.getString("fullName");
+                                String email = document.getString("email");
+                                String phoneNumber = document.getString("phoneNumber");
+                                String organization = document.getString("organization");
+
+                                // Set the text views with retrieved data
+                                if (fullName != null) usersFullName.setText(fullName);
+                                if (email != null) usersEmail.setText(email);
+                                if (phoneNumber != null) usersPhoneNo.setText(phoneNumber);
+                                if (organization != null) usersOrganization.setText(organization);
+
+                            } else {
+                                Log.d("ProfileFragment", "No such document");
+                            }
+                        } else {
+                            Log.d("ProfileFragment", "get failed with ", task.getException());
+                        }
+                    });
+        }
+        return view;
+
     }
 }
