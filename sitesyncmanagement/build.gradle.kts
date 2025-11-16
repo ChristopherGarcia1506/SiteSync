5505d6ebb9250245f315ac63b37cd21df0960364
AnthonyMancia3670 <amancia2005@gmail.com>
2025-11-16 18:38:01 -0500

    Updated gradle for new tests

--- sitesyncmanagement/build.gradle.kts
+++ sitesyncmanagement/build.gradle.kts
@@ -17,6 +17,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    android {
+        testOptions {
+            unitTests.isIncludeAndroidResources = true
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -34,6 +40,9 @@ android {
 
 dependencies {
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test:core:1.5.0")
+    testImplementation("org.robolectric:robolectric:4.11.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
