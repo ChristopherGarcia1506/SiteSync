a3847eca2961f21858454f21984d51ca53d05686
Chris Garcia <chrisgarcia911@outlook.com>
2025-11-28 20:08:13 -0500

    added edit profile screen, and added edit profile functionality

---
+++ sitesyncmanagement/src/main/java/ca/sitesync/sitesync/EditProfileFragment.java
@@ -0,0 +1,43 @@
+package ca.sitesync.sitesync;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EditProfileFragment extends Fragment {
+
+
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static EditProfileFragment newInstance(String param1, String param2) {
+        EditProfileFragment fragment = new EditProfileFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        return view;
+    }
+}
