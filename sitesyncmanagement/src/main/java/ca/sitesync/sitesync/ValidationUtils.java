725d2c197097a6e01ec64324bb90cb7e64ba1ca8
NgocLe3011 <afsa3009@gmail.com>
2025-11-28 19:54:37 -0500

    Removed phone validation in feedbackfragment and moved it to validation class

--- sitesyncmanagement/src/main/java/ca/sitesync/sitesync/ValidationUtils.java
+++ sitesyncmanagement/src/main/java/ca/sitesync/sitesync/ValidationUtils.java
@@ -5,4 +5,10 @@ public class ValidationUtils {
         String emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
         return email != null && email.matches(emailPattern);
     }
+
+    public static boolean isValidPhoneNumber(String phone) {
+        // Matches a string that consists of exactly 10 digits.
+        String phonePattern = "^[0-9]{10}$";
+        return phone != null && !phone.trim().isEmpty() && phone.matches(phonePattern);
+    }
 }
